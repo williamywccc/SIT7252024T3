baece90979f777745d9f47b9c1f30cc7f98bad1b
williamywccc <williamyipwc@gmail.com>
2025-01-11 23:16:17 +1100

    Add files via upload

--- Week7/test/test.js
+++ Week7/test/test.js
@@ -1,1 +1,13 @@
+const io = require('socket.io-client');
+const assert = require('chai').assert;
 
+describe('Socket.IO Test', () => {
+  it('should connect to the server', (done) => {
+    const socket = io('http://localhost:3000');
+    socket.on('connect', () => {
+      assert.isTrue(socket.connected);
+      socket.disconnect();
+      done();
+    });
+  });
+});

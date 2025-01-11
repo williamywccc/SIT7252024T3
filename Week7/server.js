3088e563b0970c8cca570421d02e78ee61c72b67
williamywccc <williamyipwc@gmail.com>
2025-01-11 23:19:31 +1100

    Add files via upload

---
+++ Week7/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
